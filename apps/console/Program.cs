bb352535f3c5ce9099cd3fd5d9f03c9bc6314102
kazenetu <rakudafactory@gmail.com>
2021-12-23 23:37:10 +0900

    実行用コンソールプロジェクトを追加 #1

---
+++ apps/console/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Logic;
+
+namespace console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hostName;
+            string userID;
+            string password;
+            string database;
+            if (args.Length < 4)
+            {
+                Console.WriteLine("Input parameters \"ServerName UserID Password DatabaseName [portNo]\"");
+                return;
+            }
+            hostName = args[0];
+            userID = args[1];
+            password = args[2];
+            database = args[3];
+            if (args.Length > 4)
+            {
+                var port = int.Parse(args[4]);
+                Entry.CreateSources(hostName, userID, password, database, port);
+            }
+            else{
+                Entry.CreateSources(hostName, userID, password, database);
+            }
+
+
+        }
+    }
+}
