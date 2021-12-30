162479ba410a4ad31fc2fc0dbfa354a4f28f60a9
kazenetu <rakudafactory@gmail.com>
2021-12-30 23:13:33 +0900

    エントリクラス「Generator」を追加 #3

---
+++ apps/GenerateCS/Generator.cs
@@ -0,0 +1,48 @@
+using Logic.Interface;
+using System;
+using System.IO;
+using System.Text;
+
+namespace GenerateCS
+{
+    /// <summary>
+    /// DBテーブル情報からC#ソースコード生成クラス
+    /// </summary>
+    public class Generator
+    {
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <remarks>スタティックメソッドを使用するため非公開</remarks>
+        private Generator()
+        {
+        }
+
+        /// <summary>
+        /// C#用DTOクラス生成
+        /// </summary>
+        /// <param name="table">DBテーブル情報</param>
+        /// <param name="outputPath">出力ディレクトリパス</param>
+        public static void Generate(ITable table, string outputPath)
+        {
+            try
+            {
+                // C#ソースコードを取得
+                var createCS = new Templates.CreateCS(table);
+                var csSource = createCS.TransformText();
+
+                // ファイル出力
+                var filePath = Path.Combine(outputPath, createCS.FileName);
+                using (FileStream fs = File.OpenWrite(filePath))
+                {
+                    Byte[] info = new UTF8Encoding(true).GetBytes(csSource);
+                    fs.Write(info, 0, info.Length);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
