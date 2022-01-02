b08455c1a80a21bdb63db3a9885cafeffb4b1a6d
kazenetu <rakudafactory@gmail.com>
2022-01-02 21:09:20 +0900

    InterfaceクラスをInterfaceプロジェクトに移植 #3

---
+++ apps/Interface/ITable.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+
+namespace Interface
+{
+    /// <summary>
+    /// テーブル
+    /// </summary>
+    public interface ITable
+    {
+        /// <summary>
+        /// テーブル名
+        /// </summary>
+        string Name { get; }
+
+        /// <summary>
+        /// コメント
+        /// </summary>
+        string Comment { get; }
+
+        /// <summary>
+        /// カラムリスト
+        /// </summary>
+        List<IColumn> Columns { get; }
+    }
+
+}
